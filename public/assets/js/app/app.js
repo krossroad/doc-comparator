c50c13f098278a16210d4cf8ef7703ddca70c756
krossroad <krossroad07@gmail.com>
2015-06-23 23:07:15 +0545

    Initialize app by determining the parameters for app

--- public/assets/js/app/app.js
+++ public/assets/js/app/app.js
@@ -1,9 +1,28 @@
 (function () {
 
     var AppView = function() {
+        this.recordId1 = null;
+        this.recordId2 = null;
+        this.action = null;
 
+        this.initialize = function() {
+            this.determineParams();
+        };
+
+        this.determineParams = function () {
+            var pathdump = window.location.pathname.replace(/^\/?|\/?$/, "")
+                            .split('/')
+                            .splice(1, 3);
+
+            this.recordId1    = pathdump[0];
+            this.recordId2    = pathdump[1];
+            this.action = (pathdump.length == 3) ? pathdump[2] : 'write';
+        };
+
+        this.initialize();
     };
 
     window.onload = function () {
+        var appView = new AppView();
     };
 })();
