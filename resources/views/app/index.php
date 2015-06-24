ee77f66971a749d8bfb57da9f350c158ac9a1312
krossroad <krossroad07@gmail.com>
2015-06-24 22:24:30 +0545

    Add Quill

--- resources/views/app/index.php
+++ resources/views/app/index.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <title>Comparator</title>
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="<?php echo url('assets/css/style.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo url('assets/css/quill.snow.css') ?>">
 
     <script type="text/javascript">
         var BaseConfig = {
@@ -15,27 +17,35 @@
         <h2>Campare</h2>
 
         <div class="row">
-            <div class="col-md-6 quill-wrapper" id="left-panel" >
+            <div class="col-md-6 " id="left-panel" >
+                <div class="quill-wrapper">
+                    <?php if ($action == 'write'): ?>
+                        <?php echo view('app.toolbar')->render(); ?>
+                    <?php endif ?>
 
-                <?php echo view('app.toolbar')->render(); ?>
-
-                <div style="width: 100%; " class="panel-text-view" ></div>
+                    <div style="width: 100%; " class="panel-text-view" ></div>
+                </div>
                 <?php if ($action == 'write'): ?>
-                    <button id="save-left-doc" class="save-doc" >Save</button>
+                    <button id="save-left-doc" class="save-doc push_button blue" >Save</button>
                 <?php endif ?>
             </div>
-            <div class="col-md-6 quill-wrapper" id="right-panel">
+            <div class="col-md-6" id="right-panel">
 
-                <?php echo view('app.toolbar')->render() ?>
+                <div class="quill-wrapper">
+                    <?php if ($action == 'write'): ?>
+                        <?php echo view('app.toolbar')->render() ?>
+                    <?php endif ?>
 
-                <div style="width: 100%;" class="panel-text-view" ></div>
+                    <div style="width: 100%;" class="panel-text-view" ></div>
+                </div>
                 <?php if ($action == 'write'): ?>
-                    <button id="save-right-doc" class="save-doc">Save</button>
+                    <button id="save-right-doc" class="save-doc push_button blue">Save</button>
                 <?php endif ?>
             </div>
         </div>
     </div>
     <footer>
+        <script type="text/javascript" src="<?php echo url('assets/js/lib/quill.min.js') ?>"></script>
         <script type="text/javascript" src="<?php echo url('assets/js/app/helper.js') ?>"></script>
         <script type="text/javascript" src="<?php echo url('assets/js/app/app.js') ?>"></script>
     </footer>
