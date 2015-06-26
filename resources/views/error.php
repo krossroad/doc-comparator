d327394bf13a6caf966e1709adf40e4e1f8b29a5
krossroad <krossroad07@gmail.com>
2015-06-26 10:28:53 +0545

    Add view for error-page

---
+++ resources/views/error.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ERROR</title>
+</head>
+<body>
+    <div id="container">
+        <div class="error-content">
+            <h2>ERROR</h2>
+            <div class="error-message">
+                <?php echo $errorMessage; ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
